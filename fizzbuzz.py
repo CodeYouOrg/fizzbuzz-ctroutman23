24db37937807ec798d47d9e03d52df4ae9b329f1
carson-troutman <carson.troutman@gmail.com>
2024-07-28 16:35:52 -0400

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,17 @@
 # add your code here
+count = 1
 
+while count <= 100:
+  
+  if count % 3 == 0 and count % 5 == 0: 
+      print("FizzBuzz")
+  elif count % 5 == 0:
+      print("Buzz")
+  elif  count % 3 == 0:
+      print("Fizz")
+  else:
+      print(count)
+  count += 1
+
+
+  
